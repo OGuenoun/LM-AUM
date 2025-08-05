8bc3b2e81772f730a87da21630b7e11a8bfbc5f5
Omar Guenoun <guenounomar282@gmail.com>
2025-08-05 10:04:40 -0400

    gpu config

--- job-script-ce.sh
+++ job-script-ce.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=lm_train_gpu
+#SBATCH --job-name=lm_train_ce
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=16G
-#SBATCH --time=10:00:00
-#SBATCH --gres=gpu:1               
-#SBATCH --cpus-per-task=2          
+#SBATCH --time=12:00:00
+#SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1                       
 #SBATCH --output=logs/%x-%j.out    
 #SBATCH --error=logs/%x-%j.err     
 
 
-module load python/3.11 gcc arrow cuda cudnn
+module load python/3.11 gcc arrow
 source LM_env/bin/activate
 cd ~/LM-AUM
 python LM_train_cluster.py Cross-entropy
