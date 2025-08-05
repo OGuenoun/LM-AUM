806264bc004a511def02410f2adff31693f7208c
Omar Guenoun <guenounomar282@gmail.com>
2025-08-05 10:12:21 -0400

    changed to 1/4

--- job-script-macro.sh
+++ job-script-macro.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=16G
 #SBATCH --time=12:00:00
-#SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1                       
+#SBATCH --gpus=nvidia_h100_80gb_hbm3_2g.20gb:1                       
 #SBATCH --output=logs/%x-%j.out    
 #SBATCH --error=logs/%x-%j.err     
 
